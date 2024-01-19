533e349a51fe21adf0462a14b47af3ec2b24896c
Tobibor Rahman <r.tobibor16@gmail.com>
2024-01-20 01:17:57 +0600

    client review added

--- src/components/pages/Review/Review.jsx
+++ src/components/pages/Review/Review.jsx
@@ -1,11 +1,30 @@
 import React from 'react';
+import man1 from '../../../assets/images/young-man.png';
+import man2 from '../../../assets/images/watch-man.png';
+import man3 from '../../../assets/images/old-man.png';
 
 const Review = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+	return (
+		<div className="container mx-auto py-20">
+			<h1 className="text-4xl font-bold">Client Reviews</h1>
+			<p className="border-l-[4px] rounded border-[var(--lightblue)] pl-3 mt-3">
+				The team at consistently delivered <br /> outstanding surpassing
+				expectations
+			</p>
+
+			<div className="flex gap-8 relative">
+				<div className="absolute left-[0%]">
+					<img className="w-[216px] h-[216px] " src={man1} alt="" />
+				</div>
+				<div className="absolute left-[22%]">
+					<img className="w-[216px] h-[216px] " src={man3} alt="" />
+				</div>
+				<div className="absolute left-[44%]">
+					<img className=" " src={man2} alt="" />
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Review;
